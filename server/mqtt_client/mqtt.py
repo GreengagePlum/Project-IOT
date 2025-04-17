019f14124ed79d5c375807119cad50b4e8b2e299
Sylvain PRANDO <sylvain.prando@ens.uvsq.fr>
2025-04-17 23:17:53 +0200

    Modification du client mqtt pour le lancement sous raspberry pi avec la version de paho mqtt client disponnible sous rbpi os

--- server/mqtt_client/mqtt.py
+++ server/mqtt_client/mqtt.py
@@ -1,17 +1,20 @@
 import paho.mqtt.client as mqtt
 import time
 from threading import Thread
+import random
+
+client_id = f'subscribe-{random.randint(0, 100)}'
 
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, reason_code, properties):
+def on_connect(client, userdata, flags, reason_code):#, properties): # properties supprimé
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("hello")
 
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc = mqtt.Client(client_id) # passage en paramétre d'un id aléatoire (possibilité de le remplacer par un fixe) mqtt.CallbackAPIVersion.VERSION2 n'est pas supporté
 # mqttc.suppress_exceptions = True
 mqttc.on_connect = on_connect
 # mqttc.on_message = on_message
@@ -27,7 +30,9 @@ def on_message(client, userdata, msg):
     Thread(target=op, args=[client, userdata, msg]).start()
 
 
-mqttc.connect("localhost", keepalive=5)
+mqttc.connect("127.0.0.1", 8883 , keepalive=5)  # ajout du port (1883 pas possible a cause de conflits sur les rb pi)
+mqttc.subscribe("/feeds/test")
+mqttc.on_message = on_message
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
